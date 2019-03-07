7a7dcc6367d15c3fe9c281ed2e50024f504856b2
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2019-03-07 19:58:17 +0900

    updated

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,9 @@
-const API_SERVER = process.env.BASE_URL ? (process.env.BASE_URL) : 'http://localhost:5000/';
+let API_SERVER = process.env.BASE_URL ? (process.env.BASE_URL) : 'http://localhost:5000/';
 
 if (process.env.NODE_ENV === 'production'){
     API_SERVER = 'https://mcute.herokuapp.com'
+} else {
+    
 }
 
 module.exports = API_SERVER
